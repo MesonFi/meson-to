e8b225e46e18c8c65896020342fc0f8488addaf0
Phil <qft.gtr@gmail.com>
2022-09-30 12:11:55 -0700

    add drag effect

--- src/index.js
+++ src/index.js
@@ -64,35 +64,51 @@ class MesonTo {
 
     const backdrop = doc.createElement('div')
     backdrop.style = 'position:fixed;inset:0;transition:background 0.4s;'
+    backdrop.ontouchmove = evt => evt.preventDefault()
+      
+    const container = doc.createElement('div')
+    container.style = 'z-index:10;display:flex;flex-direction:column;align-items:center;'
+    container.ontouchmove = evt => evt.preventDefault()
 
-    const popup = doc.createElement('div')
-    popup.style = 'z-index:20;max-height:100%;display:flex;flex-direction:column;align-items:center;'
     if (lgScreen) {
-      popup.style.padding = '24px 0'
-      popup.style['overflow-y'] = 'auto'
+      container.style.padding = '24px 0'
+      container.style['max-height'] = '100%'
+      container.style['overflow-y'] = 'auto'
+    } else {
+      container.style['padding-top'] = '20px'
+      container.style.transform = 'translateY(900px)'
+      container.style.transition = 'transform 0.4s'
+      container.onclick = evt => evt.stopPropagation()
     }
 
-    const container = doc.createElement('div')
-    container.style='position:relative;width:100%;max-width:440px;flex-shrink:0;background:#ecf5f0;overflow:hidden;box-shadow:0 0px 24px 0px rgb(0 0 0 / 40%)'
+    const content = doc.createElement('div')
+    content.style='position:relative;width:100%;max-width:440px;flex-shrink:0;background:#ecf5f0;overflow:hidden;box-shadow:0 0px 24px 0px rgb(0 0 0 / 40%)'
+    
+    let barWrapper
     if (lgScreen) {
-      container.style['border-radius'] = '20px'
-      container.style.opacity = '0'
-      container.style.transition = 'opacity 0.25s'
+      content.style['border-radius'] = '20px'
+      content.style.opacity = '0'
+      content.style.transition = 'opacity 0.25s'
       const close = doc.createElement('div')
       close.style = 'position:absolute;top:12px;right:16px;height:24px;font-size:28px;line-height:24px;cursor:pointer;color:#0004;'
       close.onmouseover = () => { close.style.color = '#000a' }
       close.onmouseout = () => { close.style.color = '#0004' }
       close.innerHTML = '×'
-      container.appendChild(close)
+      content.appendChild(close)
     } else {
-      container.style['border-radius'] = '20px 20px 0 0'
-      container.style.transform = 'translateY(600px)'
-      container.style.transition = 'transform 0.4s'
-      container.innerHTML = `<div style='position:absolute;top:8px;left:0;width:100%;display:flex;flex-direction:column;align-items:center'><div style='background:#444;height:4px;width:60px;border-radius:2px;overflow:hidden;'></div></div>`
+      content.style['border-radius'] = '20px 20px 0 0'
+      content.style['padding-bottom'] = '200px'
+      barWrapper = doc.createElement('div')
+      barWrapper.style = 'z-index:100;position:absolute;top:10px;left:calc(50% - 50px);cursor:pointer;'
+      barWrapper.style.transform = 'translateZ(10px)'
+      const bar = doc.createElement('div')
+      bar.style = 'background:#000;height:4px;width:60px;border-radius:2px;margin:20px;overflow:hidden;'
+      container.appendChild(barWrapper)
+      barWrapper.appendChild(bar)
     }
 
     const iframe = doc.createElement('iframe')
-    iframe.style = `width:100%;max-height:600px;overflow:hidden;`
+    iframe.style = `z-index:50;width:100%;max-height:600px;overflow:hidden;border:none;`
     iframe.src = `${this.mesonToHost}/${appId}`
     if (lgScreen) {
       iframe.style.height = 'calc(100vh - 48px)'
@@ -100,15 +116,62 @@ class MesonTo {
       iframe.style['margin-top'] = '-8px'
     } else {
       iframe.style.height = 'calc(100vh - 80px)'
+      iframe.style.transform = 'translateY(1000px)'
+      iframe.onload = () => {
+        iframe.onload = undefined
+        setTimeout(() => {
+          iframe.style.transform = ''
+        }, 100)
+      }
     }
 
     modal.appendChild(backdrop)
-    modal.appendChild(popup)
-    popup.appendChild(container)
-    container.appendChild(iframe)
+    modal.appendChild(container)
+    container.appendChild(content)
+    content.appendChild(iframe)
 
     const self = this
     this._promise = new Promise(resolve => {
+      if (barWrapper) {
+        let delta = 0
+        barWrapper.ontouchstart = evt => {
+          evt.preventDefault()
+          const initY = evt.touches[0].clientY
+          container.style.transition = 'none'
+          
+          const mask = doc.createElement('div')
+          mask.style = 'position:absolute;inset:0;z-index:100;'
+          mask.onclick = evt => evt.stopPropagation()
+          barWrapper.ontouchend = evt => {
+            if (delta < 100) {
+              container.style.transition = 'transform 0.4s'
+              container.style.transform = `translateY(200px)`
+            } else {
+              container.style.transition = 'transform 0.2s'
+              container.style['transition-timing-function'] = 'linear'
+              closer.close()
+              setTimeout(() => {
+                container.style['transition-timing-function'] = 'ease'
+                container.style.transition = 'transform 0.4s'
+              }, 200)
+            }
+            evt.preventDefault()
+            modal.removeChild(mask)
+            barWrapper.ontouchmove = null
+            barWrapper.ontouchend = null
+          }
+          barWrapper.ontouchmove = evt => {
+            evt.preventDefault()
+            delta = evt.touches[0].clientY - initY
+            if (delta < -100) {
+              delta = -100
+            }
+            container.style.transform = `translateY(${200+delta}px)`
+          }
+          modal.appendChild(mask)
+        }
+      }
+
       const closer = {
         blocked: false,
         block(blocked = true) {
@@ -117,14 +180,15 @@ class MesonTo {
         close() {
           if (this.blocked) {
             iframe.contentWindow.postMessage({ source: 'app', data: { closeBlocked: true } }, self.mesonToHost)
+            container.style.transform = `translateY(200px)`
             return
           }
-          backdrop.style.background = 'transparent'
           if (lgScreen) {
-            container.style.opacity = '0'
+            content.style.opacity = '0'
           } else {
-            container.style.transform = 'translateY(600px)'
+            container.style.transform = 'translateY(900px)'
           }
+          backdrop.style.background = 'transparent'
           setTimeout(() => {
             doc.body.removeChild(modal)
           }, 400)
@@ -143,9 +207,9 @@ class MesonTo {
       setTimeout(() => {
         backdrop.style.background = '#0006'
         if (lgScreen) {
-          container.style.opacity = '1'
+          content.style.opacity = '1'
         } else {
-          container.style.transform = 'translateY(0)'
+          container.style.transform = 'translateY(200px)'
         }
       }, 0)
     })
